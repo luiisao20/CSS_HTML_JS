800677d8eef65c54944425162692efd1dda91a2e
Lucho <bravo.luis.1995@gmail.com>
2023-02-10 19:55:26 -0500

    Agregado gestor de errores si el nick está vacío o no se escoge un campo en el tamaño

--- Principal/js/app.js
+++ Principal/js/app.js
@@ -3,16 +3,29 @@ JS para la comprobacion de datos del formulario de entrada
 */
 
 // Iniciando variables, objetos y DOM
-const buttonJugar = document.getElementById('jugar');
+const nickInput = document.getElementById('nick');
+const tamanoInput = document.getElementById('tamano');
+const formEntrada = document.getElementById('formEntrada');
+const error = document.getElementById('error');
 
 // Funciones de evento
-function comprobarForm() {
-    console.log('Comprobamos el formulario');
-}
-function ejecutarAccion() {
-    console.log('Ejecutar accion');
-    buttonJugar.removeEventListener('click', ejecutarAccion);
+function comprobarForm(event) {
+    // Comprobar cambios
+    if (nickInput.value.length == 0){
+        console.log('No hay nick');
+        nickInput.focus();
+        event.preventDefault();
+        error.innerText= 'El campo de nick no puede estar vacío'
+        return false
+    } else if(tamanoInput.value == '0'){
+        console.log('No se ha seleccionado tamaño de panel');
+        tamanoInput.focus();
+        event.preventDefault();
+        error.innerText= 'Tienes que escojer el tamaño del juego'
+        return false;
+    }
+    return true;
 }
 
-buttonJugar.addEventListener('click', comprobarForm);
-buttonJugar.addEventListener('click', ejecutarAccion);
+// Inicio de carga de eventos
+formEntrada.addEventListener('submit', comprobarForm);
